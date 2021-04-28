7a0efea766fb653caed8d635440d574df8e33a51
Jones <git@jonasscheiwiller.ch>
2021-04-28 10:33:52 +0200

    [Update] Add mobile nav toggle and 4th miro board link

--- docs/_javascript/main.js
+++ docs/_javascript/main.js
@@ -1,3 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('Hello Bulma!');
+
+  // add addEventListeners for burger
+  const burger = document.querySelector("[data-burger]");
+  const nav = document.querySelector("[data-nav]");
+
+  burger.addEventListener('click', () => {
+    console.log('toggle nav');
+    burger.classList.toggle('is-active');
+    nav.classList.toggle('is-visible');
+  });
 });
+
--- docs/lib/main.js
+++ docs/lib/main.js
@@ -2,4 +2,14 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   console.log('Hello Bulma!');
+
+  // add addEventListeners for burger
+  var burger = document.querySelector("[data-burger]");
+  var nav = document.querySelector("[data-nav]");
+
+  burger.addEventListener('click', function () {
+    console.log('toggle nav');
+    burger.classList.toggle('is-active');
+    nav.classList.toggle('is-visible');
+  });
 });
