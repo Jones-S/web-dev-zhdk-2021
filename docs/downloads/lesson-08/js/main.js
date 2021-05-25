d4379d2dedcc88a8075939bc32c6324038200452
Jonas <git@jonasscheiwiller.ch>
2021-05-25 22:15:22 +0200

    [Update] Add playground-files for lesson 08, add show-emoji-click base for exercise

---
+++ docs/downloads/lesson-08/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('The DOM is ready.');
+
+  /**
+  select and console.log div with class .select-me
+  select and console.log h2 inside of .select-me
+  select and console.log the last table cell
+  */
+
+});
