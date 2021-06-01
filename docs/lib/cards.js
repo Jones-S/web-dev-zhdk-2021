e85eaaaade019ab588f9ef798311bcb44fe3aa30
Jonas <git@jonasscheiwiller.ch>
2021-06-02 00:53:21 +0200

    [WIP] Add student-cards and build page slider

---
+++ docs/lib/cards.js
@@ -0,0 +1,107 @@
+'use strict';
+
+let buttons = {
+  prev: false,
+  next: false
+};
+let iframe;
+let pageSlider = false;
+
+class Slider {
+  constructor(pages) {
+    this.pages = pages;
+    this.currentPageIndex = 0;
+  }
+  
+  callNext() {
+    this.currentPageIndex += 1;
+    if (this.currentPageIndex > this.pages.length) this.currentPageIndex = 0;
+    console.log('this.currentPage: ', this.currentPageIndex);
+  }
+  
+  callPrevious() {
+    this.currentPageIndex -= 1;
+    if (this.currentPageIndex < 0) this.currentPageIndex = this.pages.length - 1;
+    console.log('this.currentPage: ', this.currentPageIndex);
+  }
+
+  async fetchPage() {
+    console.log('...fetching');
+    let page;
+
+    await fetch('./cards/bin-martig/index.html')
+      .then(function (response) {
+        return response.text();
+      })
+      .then(function (html) {
+        // Convert the HTML string into a document object
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        console.log('doc: ', doc)
+        page = doc;
+      })
+      .catch(function (err) {
+        console.warn('Something went wrong.', err);
+      });
+
+    return page;
+  }
+}
+
+async function fetchCards() {
+  let cards;
+  // fetching all student cards by reading generated .json file from directories
+  await fetch('./cards/cards.json')
+    .then(response => response.json())
+    .then(json => {
+      cards = json;
+    });
+  
+  return cards;
+}
+
+async function next () {
+  if (pageSlider) {
+    pageSlider.callNext();
+    const page = await pageSlider.fetchPage();
+    showPage(page);
+  }
+}
+
+function prev () {
+  if (pageSlider) {
+    pageSlider.callPrevious();
+    const page = pageSlider.fetchPage();
+    showPage(page);
+  }
+}
+
+function showPage(page) {
+  console.log('page: ', page);
+  // iframe.innerHTML = page;
+
+  const iframeDoc = iframe.contentDocument ? iframe.contentDocument : iframe.contentWindow.document; // IE compatibility
+
+  iframeDoc.open('text/html');
+  iframeDoc.write(page.body.innerHTML);
+  iframeDoc.close();
+}
+
+function initUi() {
+  iframe = document.querySelector('[data-content]');
+  buttons.prev = document.querySelector('[data-button="prev"]');
+  buttons.next = document.querySelector('[data-button="next"]');
+
+  buttons.prev.addEventListener('click', prev);
+  buttons.next.addEventListener('click', next);
+}
+
+
+
+document.addEventListener('DOMContentLoaded', async function () {
+  console.log('... Fetching Cards');
+  initUi();
+
+  const cards = await fetchCards();
+  pageSlider = new Slider(cards);
+});
---
+++ docs/pages/writeCardsToJson.js
@@ -0,0 +1,44 @@
+/* Run this js with node to write all student directories to a JSON-file */
+
+const path = require('path');
+const fs = require('fs');
+
+const directoryPath = path.join(__dirname, 'cards');
+
+fs.readdir(directoryPath, function (err, directories) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+
+    const cards = directories.filter(dir => {
+      return dir != '.DS_Store';
+    })
+    
+    const data = [];
+    cards.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file);
+        data.push({
+            path: `./${file}`
+        });
+    });
+
+    const json = JSON.stringify(data, null, null);
+    const jsonSizeKB = Math.round((Buffer.byteLength(json, 'utf8') / 1024) * 100) / 100;
+    const fileName = 'cards.json';
+    const savePath = `${__dirname}/cards`;
+    console.info(`Writing ${fileName} (Length: ${json.length}, Size: ${jsonSizeKB}kB)`);
+
+    if (!fs.existsSync(savePath)) {
+        fs.mkdirSync(savePath)
+    }
+
+    return fs.writeFile(`${savePath}/${fileName}`, json, 'utf-8', (err) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log(`✅💾 Wrote cards to json successfully!`);
+        }
+    })
+});
+
